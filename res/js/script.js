696e423475cae5bf3a0ad19793d9dc95f74d8f0b
tawhidmonowar <tawhidmonowar@gmail.com>
2022-09-10 14:55:40 +0600

    10-09-2022

--- res/js/script.js
+++ res/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
     $(window).scroll(function(){
-        if($(this).scrollTop() > 40){
+        if($(this).scrollTop() > 30){
             $('#top-button').fadeIn();
         } else {
             $('#top-button').fadeOut();
